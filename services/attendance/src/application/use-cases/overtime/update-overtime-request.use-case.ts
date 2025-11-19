36c4a87c72e20e60b2a7287687a5ed5e96edd9e5
Trần Vũ Kiên <kientvhe172514@fpt.edu.vn>
2025-11-19 12:59:34 +0700

    response data to update

--- services/attendance/src/application/use-cases/overtime/update-overtime-request.use-case.ts
+++ services/attendance/src/application/use-cases/overtime/update-overtime-request.use-case.ts
@@ -75,7 +75,7 @@ export class UpdateOvertimeRequestUseCase {
     }
 
     return ApiResponseDto.success(
-      undefined,
+      updated,
       'Overtime request updated successfully',
     );
   }
